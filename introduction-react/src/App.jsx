2f2b3c3f3315f28d72b39b86c8ede3b3ce05f3c4
LaigleJ <you@example.com>
2024-12-17 11:15:55 +0100

    clean code

--- introduction-react/src/App.jsx
+++ introduction-react/src/App.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Header } from './component/containers/header/Header'
 import { Footer } from './component/containers/footer/Footer'
