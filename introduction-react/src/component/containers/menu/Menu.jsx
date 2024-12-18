3cc4c7d4c3d31e375e9a35f5348f47f45047ee30
LaigleJ <you@example.com>
2024-12-18 14:53:55 +0100

    add css

--- introduction-react/src/component/containers/menu/Menu.jsx
+++ introduction-react/src/component/containers/menu/Menu.jsx
@@ -11,9 +11,11 @@ export function Menu() {
     <>
       <Button gereclique={toggleMenu}>{isVisible ? "hide" : "show"}</Button>
 
-      <section className= { isVisible ? "visible" : "hidden" }>
-      <h3>Menu Secret</h3>
-      <ol>
+      <section className={`${
+            isVisible ? "hidden" : "block"
+          } transition-all duration-300 ease-in-out mt-6 bg-gray-500 shadow-xl rounded-lg p-6 w-64`}  >
+      <h3 className="text-xl text-center">Menu Secret</h3>
+      <ol className="list-decimal gap-1">
         <li>Nourir le chien</li>
         <li>Faire mes devoirs</li>
         <li>Essayer des trucs en React</li>
